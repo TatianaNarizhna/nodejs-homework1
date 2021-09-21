8bb80e614d45e3da1bbb5b08fd9687439f8066a6
TatianaNarizhna <narizhna.tania@gmail.com>
2021-09-21 22:50:50 +0300

    work

--- contacts.js
+++ contacts.js
@@ -1,15 +1,24 @@
-const fs = require("fs").promises;
+const fs = require('fs/promises');
 const path = require('path');
+const crypto = require('crypto');
 
+const contactsPath = path.join(__dirname, 'db', 'contacts.json');
 
-const contactsPath = path.resolve('db', 'contacts.json');
-console.log(contactsPath);
-
-
-// TODO: задокументировать каждую функцию
-function listContacts() {
-    // ...твой код
+const readContacts = async () => {
+    try {
+        const result = await fs.readFile(contactsPath, 'utf8')
+        const contacts = JSON.parse(result);
+        return contacts;
+    } catch (error) {
+        console.error(error);
+    }
 }
+
+async function listContacts() {
+ return readContacts();
+};
+
+
 
 function getContactById(contactId) {
     // ...твой код
@@ -19,9 +28,22 @@ function removeContact(contactId) {
     // ...твой код
 }
 
-function addContact(name, email, phone) {
-    // ...твой код
-}
+async function addContact(name, email, phone ) {
+    try {
+        const contacts = await readContacts();
+        const newContact = {
+            id: crypto.randomUUID(),
+            name,
+            email,
+            phone,
+        }
+        contacts.push(newContact);
+        await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2), );
+        return newContact;
+    } catch (error) {
+        console.error(error);
+    }
+};
 
 module.exports = {
     listContacts,
@@ -29,3 +51,15 @@ module.exports = {
     removeContact,
     addContact,
 }
+
+// function listContacts() {
+//     fs.readFile(contactsPath, 'utf8', (error, data) => {
+//         if (error) {
+//             console.log(error)
+//         }
+//         console.log(data)
+//     })
+// }
+
+// const contactsPath = path.resolve('db', 'contacts.json');
+// console.log(contactsPath);
--- index.js
+++ index.js
@@ -1,3 +1,55 @@
-console.log('helloooooo')
+const { Command } = require('commander');
+const chalk = require('chalk');
+const { 
+    listContacts,
+    getContactById,
+    removeContact,
+    addContact, } = require('./contacts');
+
+const program = new Command();
+
+program
+  .option('-a, --action <type>', 'choose action')
+  .option('-i, --id <type>', 'user id')
+  .option('-n, --name <type>', 'user name')
+  .option('-e, --email <type>', 'user email')
+  .option('-p, --phone <type>', 'user phone');
+
+program.parse(process.argv);
+
+const argv = program.opts();
+
+// TODO: рефакторить
+function invokeAction({ action, id, name, email, phone }) {
+  switch (action) {
+    case 'list':
+        listContacts()
+        .then((contacts) => console.table(contacts))
+        .catch(console.error);
+      break;
+
+    case 'get':
+      // ... id
+      break;
+
+    case 'add':
+        addContact(name, email, phone)
+         .then((contact) => {
+            console.log(chalk.green('Add new contact!'))
+            console.log(contact);
+         })
+         .catch(console.error)
+      break;
+
+    case 'remove':
+      // ... id
+      break;
+
+    default:
+      console.warn('\x1B[31m Unknown action type!');
+  }
+}
+
+invokeAction(argv);
 
 
